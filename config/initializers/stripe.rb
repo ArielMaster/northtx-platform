bc674845fa16e3f082a880ea0f8c937c6309feb4
Ariel Falcon <arielfalcon21@users.noreply.github.com>
2026-05-06 01:30:21 -0500

    stripe setup

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,8 @@
 # config/initializers/stripe.rb
-Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+
+# Production
+Rails.configuration.stripe = {
+  publishable_key: ENV["STRIPE_PUBLISHABLE_KEY"],
+  secret_key: ENV["STRIPE_SECRET_KEY"]
+}
