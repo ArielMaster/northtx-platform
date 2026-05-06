ea1d67c3005ad1f40c706f4ca8be2030920b9720
Ariel Falcon <arielfalcon21@users.noreply.github.com>
2026-05-06 18:59:35 -0500

    stribe.rb re-configure

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,10 +1,10 @@
 # config/initializers/stripe.rb
-Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
 
 # Production
-# Rails.configuration.stripe = {
-#   publishable_key: ENV["STRIPE_PUBLISHABLE_KEY"],
-#   secret_key: ENV["STRIPE_SECRET_KEY"]
-# }
+Rails.configuration.stripe = {
+  publishable_key: ENV["STRIPE_PUBLISHABLE_KEY"],
+  secret_key: ENV["STRIPE_SECRET_KEY"]
+}
 
 # Stripe.api_key = Rails.configuration.stripe[:secret_key]
