b8c89f4fce57b70804b4b148d2d30201e2464c39
Ariel Falcon <arielfalcon21@users.noreply.github.com>
2026-05-06 20:58:16 -0500

    stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,7 @@
 # config/initializers/stripe.rb
-Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+
+Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key) || ENV["STRIPE_SECRET_KEY"]
 
 # Production
 # Rails.configuration.stripe = {
