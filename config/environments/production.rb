f607b2ea97b2c31e1efa215d03abefc921a857df
Ariel Falcon <arielfalcon21@users.noreply.github.com>
2026-05-06 19:28:35 -0500

    sign out link added to nav

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "https://madre-c9549827c462.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://padre-51c71e7c5e5d.herokuapp.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
